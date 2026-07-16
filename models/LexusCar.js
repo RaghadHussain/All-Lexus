7bd2b575d97a18a5eb1dbac5125439d2737ac167
Your Name <you@example.com>
2026-07-16 11:21:06 +0300

    Create LexusCar Model

---
+++ models/LexusCar.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+
+const lexusCarSchema = new mongoose.Schema({
+    dealer: {
+        type: String,
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    condition: {
+        type: String,
+        required: true,
+        enum: ['New', 'Used']
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    mileage: {
+        type: Number,
+        required: true
+    },
+    fuelType: {
+        type: String,
+        required: true
+    },
+    engine: {
+        type: String,
+        required: true
+    },
+    hoursepower: {
+        type: Number,
+        required: true
+    },
+    carImage: {
+        type: String
+    },
+    status: {
+        type: String,
+        required: true,
+        enum: ['Available', 'Sold']
+    }
+},{timestamps:true})
+
+const LexusCar = mongoose.model('LexusCar', lexusCarSchema)
+module.exports = LexusCar
